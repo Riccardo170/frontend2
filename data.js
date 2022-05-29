5d9a20e3c41a06eb7b0fc3ed2d907cec85d72a88
riccardo <riccagallo67@gmail.com>
2022-05-29 22:31:55 +0200

    finire bottone

--- data.js
+++ data.js
@@ -1,188 +1,77 @@
-// var url= 'http://localhost:8080/backrest/index.php';
-// var nexId = 10006;
-// var next;
-// var last;
-// var prev;
-// var first;
-// var response=null;
-// var id;
 
-// $(document).ready(function () {
-
-//   // url iniziale
-//   chiamataServer(url+"?page=0&size=20");
-// //elimina
-//   $("body").on("click", "#btn-delete", function () {
-//     $(this).parents("tr").fadeOut("fast");
-//     var td = $(this).parent("td");
-//     var id = td.data("id");
-//     console.log(id);
-//     $.ajax({
-//       url: url+'/?id='+id,// url + id dell'utente selezionato
-//       type: "delete", //si dice di vooler cancellare dal db
-//       success: function(data){
-        
-
-//         displayTable(chiamataServer(url+"?page="+response['page']['number']+"&size=20"));}//e se ha successo(significa che è stata cancellata) si da la url all display table
-//   })
-//   });
-  
-
-// //aggiungi
-//   $("body").on("click", "#btn-add", function () {
-//     var firstName = $('#firstName').val();
-//     console.log(firstName);
-//     var lastname = $('#lastname').val();
-//     console.log(lastname);
-
-//     var nuovo = {
-//       "id": nexId,
-//       "firstName": firstName,
-//       "lastName": lastname,
-//       "gender": "M",
-
-//     }
-//     $.ajax({
-//       type: "POST",
-//       url:  'http://localhost:8081/backrest/index.php/?nome='+nuovo.firstName+'&cognome='+nuovo.lastName,
-
-//       contentType: "application/json",
-      
-
-//       success: function () {
-//           var last = response["_links"]["last"]["href"];
-//           console.log(last);
-//           chiamataServer(last);
-//       }
-//   });
-    
-
-//     //chiusura dopo aggiungi
-//     var modal= $('#exampleModal');
-//     modal.modal("hide");
-
-
-//   });
-
-//   //modifica
-//   $("#confirm-modifica").click(function () {
-
-
-//     var data = {
-//       "id": id,
-//       "firstName": $("#nome-m").val(),
-//       "lastName": $("#cognome-m").val(),
-//       "gender": "M"
-//     }
-
-//     $.ajax({
-//       type: "PUT", //si dice di vooler aggiornare dal db
-//       url: 'http://localhost:8081/backrest/index.php/?id='+data.id+'&nome='+data.firstName+'&cognome='+data.lastName,// url + id dell'utente selezionato
-//         success: function(data){
-//           chiamataServer(response['_links']['self']['href']);
-
-          
-//         }//e se ha successo(significa che è stata cancellata) si da la url all display table
-//     })
-//     var modal= $('#modalmodifica');
-//     modal.modal("hide");
-
-
-
-    
-//   });
-
-
-//   $("body").on("click", "#btn-modifica", function () {
-
-
-//     $(this).parents("tr").fadeOut("fast");
-//     var td = $(this).parent("td");
-//     id = td.data("id");
-
-    
-//     $.get('http://localhost:8081/backrest/index.php/?id=' + id, function(data) {
-
-//       $("#nome-m").val(data.firstName);
-//       $("#cognome-m").val(data.lastName);
-
-//     });
-
-//   });
-  
-// });
-
-// function chiamataServer(link) {
-//   $.ajax({
-//     url: link,
-//     success: function( responseData ) {
-//       response=responseData;
-//       displayTable(response["_embedded"]["employees"]);
-//       console.log(response)
-//       //displayPagination(response["page"], response["_links"]);
-//       if(response["page"]["number"]!=response["page"]["totalPages"]-1){
-//         next = response['_links']['next']['href']
-//       }
-//       last=response['_links']['last']['href']
-//       prev=response['_links']['prev']['href']
-//       first=response['_links']['first']['href']
-      
-//     },
-//     dataType: 'json'
-//   });
-
-// }
-
-
-// $('#next').click(function(){
-//   console.log(next);
-//   chiamataServer(next);
-// });
-
-// $('#last').click(function(){
-//   console.log(last);
-//   chiamataServer(last);
-// });
-
-// $('#prev').click(function(){
-//   console.log("prev:"+prev);
-//   chiamataServer(prev);
-// });
-
-// $('#first').click(function(){
-//   console.log(first);
-//   chiamataServer(first);
-// });
-
-// function displayTable(dati) {
-//   var r = '';
-//   $.each(dati, function (i, value) {
-//     r += '<tr>';
-//     r += '<td>' + value.id + '</td>';
-//     r += '<td>' + value.firstName + '</td>';
-//     r += '<td>' + value.lastName + '</td>';
-//     r += '<td data-id=' + value.id + '> <button type="button" class="btn btn-danger" id="btn-delete">Elimina</button>' + '</td>';
-//     r += '<td data-id=' + value.id + '> <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modalmodifica" id="btn-modifica">Modifica</button>' + '</td>';
-//     r += '<tr>' + '</tr>';
-//   });
-//   $("tbody").html(r);
-// }
-
+var editor;
+ 
 $(document).ready(function() {
-  $('#example').DataTable( {
-      "processing": true,
-      "serverSide": true,
-      "ajax": {
-          "url": "index.php",
-          "type": "POST"
+    editor = new $.fn.dataTable.Editor( {
+        ajax: "index.php",
+        table: "#example",
+        fields: [ {
+                label: "first_name:",
+                name: "users.first_name"
+            }, {
+                label: "Last name:",
+                name: "users.last_name"
+            }, {
+                label: "gender:",
+                name: "users.gender"
+            }, {
+                label: "birth_date:",
+                name: "users.birth_date",
+                type: "datetime"
+            }, {
+                label: "hire_date:",
+                name: "users.hire_date",
+                type: "datetime"
+            },{
+                name: "users.removed_date",
+                type: "hidden"
+            }
+        ]
+    } );
+ 
+    var table = $('#example').DataTable( {
+        dom: "Bfrtip",
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+        "url": "index.php",
+        "type": "POST"
       },
-      "columns": [
-          { "data": "first_name" },
-          { "data": "last_name" },
-          { "data": "position" },
-          { "data": "office" },
-          { "data": "start_date" },
-          { "data": "salary" }
-      ]
-  } );
+        columns: [
+            { data: "id"},
+            { data: "first_name" },
+            { data: "last_name" },
+            { data: "gender" },
+            { data: "birth_date" },
+            { data: "hire_date" }
+        ],
+        select: true,
+        buttons: [
+            { extend: "create", editor: editor },
+            { extend: "edit",   editor: editor },
+            {
+                extend: "selected",
+                text: 'Delete',
+                action: function ( e, dt, node, config ) {
+                    var rows = table.rows( {selected: true} ).indexes();
+ 
+                    editor
+                        .hide( editor.fields() )
+                        .one( 'close', function () {
+                            setTimeout( function () { // Wait for animation
+                                editor.show( editor.fields() );
+                            }, 500 );
+                        } )
+                        .edit( rows, {
+                            title: 'Delete',
+                            message: rows.length === 1 ?
+                                'Are you sure you wish to delete this row?' :
+                                'Are you sure you wish to delete these '+rows.length+' rows',
+                            buttons: 'Delete'
+                        } )
+                        .val( 'users.removed_date', (new Date()).toISOString().split('T')[0] );
+                }
+            }
+        ]
+    } );
 } );
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $metodo = $_SERVER['REQUEST_METHOD'];
-$index="http://localhost:8080/backrest/index.php";
-$servername = "172.17.0.1:3306";
+$index="http://localhost:8081/frontend2/index.php";
+$servername = "172.17.0.1:3307";
 $user = "root";
 $pass = "ricca";
 $db="mydb";
@@ -10,65 +10,74 @@ $db="mydb";
 $conn =  mysqli_connect($servername, $user, $pass, $db) or die("Connessione non riuscita". mysqli_connect_error());
 
 
-
-if ($metodo === 'GET') {
-    header("Access-Control-Allow-Origin: *");
-    header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Methods: POST");
-    header("Access-Control-Max-Age: 3600");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    $page=$_GET['page'];
-    $size=$_GET['size'];
-    $sql = "SELECT id,birth_date,first_name,last_name, gender,hire_date from employees limit ".$page*$size.",".$size;
-    $result = mysqli_query ($conn, $sql) or //risultato
-    die ("Query fallita " . mysqli_error($conn) . " " . mysqli_errno($conn));
-    $employees=array();
-    while ($row = mysqli_fetch_array ($result, MYSQLI_NUM)){
-      array_push($employees, array("id"=>$row[0], "birthDate"=>$row[1], "firstName"=>$row[2], "lastName"=>$row[3], "gender"=>$row[4], "hireDate"=>$row[5], 
-                  'links_' =>array( 'self'=>array('href'=>$index."?id=".$row[0]), 
-                              'employee'=>array('href'=>$index."?id=".$row[0]))
-                )); 
-    }
-    $imp=array();
-    $imp['_embedded']['employees']=$employees;
-    $sql = "SELECT count(id) as count from employees";
-    $result = mysqli_query ($conn, $sql) or //risultato
-    die ("Query fallita " . mysqli_error($conn) . " " . mysqli_errno($conn));
-    $employees=array();
-    while ($row = mysqli_fetch_array ($result, MYSQLI_NUM)){
-        $tot=$row[0];
-    }
-    $links ['first']['href']='http://localhost:8081/backrest/index.php?page=0&size='.$size; 
-    $links ['self']['href']=$index.'?page='.$page.'&size='.$size;
-    $links ['next']['href']=$index.'?page='.($page+1).'&size='.$size;
-    $links ['prev']['href']=$index.'?page='.($page-1).'&size='.$size;
-    $links ['last']['href']=$index.'?page='.intval($tot/20).'&size='.$size;
-
-    $pages = array('size'=>$size, 'totalElements'=>$tot, 'totalPages'=>intval($tot/20), 'number'=>intval($page));
-
-    $imp['_links']=$links;
-    $imp['page']=$pages;
-    echo json_encode($imp,JSON_UNESCAPED_SLASHES);
-    $sql = "SELECT * from employees";
-    $result = $conn->query($sql);
-
-   
-} else 
 if ($metodo == "POST"){
-    $nome= $_GET['nome'];  
-    $cognome= $_GET['cognome'];  
-    $sql = "INSERT INTO employees (first_name, last_name) VALUES ('$nome','$cognome')";
-    $result = mysqli_query ($conn, $sql) or die ("Query fallita " . mysqli_error($conn) . " " . mysqli_errno($conn));  
+
+  $start=$_POST['start'];
+  $lenght=$_POST['length'];
+  $cerca=$_POST['search']['value'];
+  $ord=$_POST['order']['0']['dir'];
+  $num=$_POST['order']['0']['column'];
+  
+  $num=$num + 1;
+  $a = array();
+  $Selectall = "SELECT * FROM employees WHERE id like '%{$cerca}%' 
+  OR birth_date like '%{$cerca}%' 
+  OR first_name like '%{$cerca}%'
+  OR last_name like '%{$cerca}%'
+  OR gender like '%{$cerca}%'
+  OR hire_date like '%{$cerca}%' order by {$num} {$ord} limit {$start},{$lenght}"; //select 
+  $Selectallr = mysqli_query ($conn, $Selectall) or //risultato
+  die ("Query fallita 0 " . mysqli_error($conn) . " " . mysqli_errno($conn));
+ 
+  $count = "SELECT count(id) FROM employees  "; //select 
+  $countr = mysqli_query ($conn, $count) or //risultato
+  die ("Query fallita 0 " . mysqli_error($conn) . " " . mysqli_errno($conn));
+ 
+  $countf = "SELECT count(id) FROM employees WHERE id like '%{$cerca}%' 
+  OR birth_date like '%{$cerca}%' 
+  OR first_name like '%{$cerca}%'
+  OR last_name like '%{$cerca}%'
+  OR gender like '%{$cerca}%'
+  OR hire_date like '%{$cerca}%'
+  "; //select 
+  $countfr = mysqli_query ($conn, $countf) or //risultato
+  die ("Query fallita 0 " . mysqli_error($conn) . " " . mysqli_errno($conn));
+
+
+
+
+while ($row = mysqli_fetch_array ($countr, MYSQLI_NUM)) 
+{
+$a['recordsTotal']=$row['0'];
 }
-  if ($metodo == "PUT"){
-    $id= $_GET['id'];  
-    $nome= $_GET['nome'];  
-    $cognome= $_GET['cognome'];  
-    $sql = "UPDATE employees SET first_name = '$nome' , last_name= '$cognome' WHERE id = '$id'";
-    $result = mysqli_query ($conn, $sql) or die ("Query fallita " . mysqli_error($conn) . " " . mysqli_errno($conn));  
+
+while ($rowi = mysqli_fetch_array ($countfr, MYSQLI_NUM)) 
+{
+$a['recordsFiltered']=$rowi['0'];
 }
-  if ($metodo == "DELETE"){
-  $id= $_GET['id'];         
-$sql = " DELETE from employees where  id = ".$id;
-$result = mysqli_query ($conn, $sql) or die ("Query fallita " . mysqli_error($conn) . " " . mysqli_errno($conn));  
+
+
+
+  $a["data"]=array();
+  while ($row = mysqli_fetch_array ($Selectallr, MYSQLI_NUM)) 
+  {
+ $dipendente=array(
+ 
+    "id"=>$row['0'],
+  "birth_date"=>$row['1'],
+  "first_name"=>$row['2'],
+  "last_name"=>$row['3'],
+  "gender"=>$row['4'],
+  "hire_date"=>$row['5']
+ );
+ array_push($a["data"], $dipendente);
+  }
+
+  echo json_encode($a);
+
+
+
+
 }
+
+?>    
